3492564f61b7a66572e7e366760976bfbd951eaa
User ulyana <lpkn8103@gmail.com>
2025-03-26 00:17:00 +0300

    интерфейс

--- WindowsForms/Form1.Designer.cs
+++ WindowsForms/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            pictureBox1 = new PictureBox();
+            button1 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            comboBox1 = new ComboBox();
+            label1 = new Label();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(37, 81);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(705, 437);
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            button1.Location = new Point(21, 7);
+            button1.Name = "button1";
+            button1.Size = new Size(234, 55);
+            button1.TabIndex = 1;
+            button1.Text = "Загрузить фото";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(231, -358);
+            button4.Name = "button4";
+            button4.Size = new Size(105, 79);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(326, 7);
+            button5.Name = "button5";
+            button5.Size = new Size(56, 55);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.Location = new Point(261, 7);
+            button6.Name = "button6";
+            button6.Size = new Size(59, 55);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Image = (Image)resources.GetObject("button7.Image");
+            button7.Location = new Point(388, 7);
+            button7.Name = "button7";
+            button7.Size = new Size(53, 55);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Image = (Image)resources.GetObject("button8.Image");
+            button8.Location = new Point(447, 7);
+            button8.Name = "button8";
+            button8.Size = new Size(58, 55);
+            button8.TabIndex = 8;
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Чёрно-белый фильтр", "Ретро фильтр", "Зернистось", "Винтаж", "Тусклость ", "Яркость" });
+            comboBox1.Location = new Point(521, 31);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(221, 29);
+            comboBox1.TabIndex = 11;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI Semibold", 12F, FontStyle.Bold, GraphicsUnit.Point, 204);
+            label1.Location = new Point(521, 7);
+            label1.Name = "label1";
+            label1.Size = new Size(133, 21);
+            label1.TabIndex = 12;
+            label1.Text = "Выбрать фильтр";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.DarkSalmon;
+            ClientSize = new Size(836, 547);
+            Controls.Add(label1);
+            Controls.Add(comboBox1);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private ComboBox comboBox1;
+        private Label label1;
     }
 }
